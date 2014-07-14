bb234221898cfa7dbf837f186008e4fc41f324cd
Kent Fredric <kentfredric@gmail.com>
2014-07-15 00:53:32 +1200

    Build results of 77ba97d (on master)

--- lib/Dist/Zilla/Role/BundleDeps.pm
+++ lib/Dist/Zilla/Role/BundleDeps.pm
@@ -1,4 +1,4 @@
-use 5.008; # utf8
+use 5.008;    # utf8
 use strict;
 use warnings;
 use utf8;
@@ -56,7 +56,7 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 
 
-use Moose::Role;
+use Moose::Role qw( around );
 
 sub _bundle_alias {
   my ($self) = @_;
@@ -68,11 +68,11 @@ sub _bundle_alias {
 }
 
 sub _extract_plugin_prereqs {
-  my ( $self, @config ) = @_;
+  my ( undef, @config ) = @_;
   require CPAN::Meta::Requirements;
   my $reqs = CPAN::Meta::Requirements->new();
   for my $item (@config) {
-    my ( $name, $module, $conf ) = @{$item};
+    my ( undef, $module, $conf ) = @{$item};
     my $version = 0;
     $version = $conf->{':version'} if exists $conf->{':version'};
     $reqs->add_string_requirement( $module, $version );
@@ -119,7 +119,7 @@ sub bundledeps_defaults {
 }
 
 around bundle_config => sub {
-  my ( $orig, $self, $section, @rest ) = @_;
+  my ( $orig, $self, $section, ) = @_;
   my $myconf = $self->bundledeps_defaults;
   for my $param (qw( phase relationship )) {
     my $field = 'bundledeps_' . $param;
