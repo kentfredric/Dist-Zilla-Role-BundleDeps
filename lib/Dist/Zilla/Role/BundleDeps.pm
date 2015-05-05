e746d739443813d9c855b99ab5338199b289a65c
Kent Fredric <kentfredric@gmail.com>
2015-05-06 10:56:42 +1200

    v0.002002
     [Dependencies::Stats]
     - Dependencies changed since 0.002001, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1)
     - develop: +10 ↑3 -2 (recommends: +1, suggests: ↑2)
     - runtime: ↓1 -1
     - test: +1 (recommends: +1 ↑1 ↓1)
    
     [Documentation]
     - Crossreference Dzil::Plugin::BundleInspector (Thanks Olivier Mengué/DOLMEN)
     - Closes #1
    
     [Metadata]
     - show 1x contributor
     - author = cpan

--- lib/Dist/Zilla/Role/BundleDeps.pm
+++ lib/Dist/Zilla/Role/BundleDeps.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Role::BundleDeps;
 
-our $VERSION = '0.002002';
+our $VERSION = '0.002003';
 
 # ABSTRACT: Automatically add all plugins in a bundle as dependencies
 
