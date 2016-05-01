e76c1f17d9460e9b370f782022b6a550ef3c47fe
Kent Fredric <kentfredric@gmail.com>
2016-05-01 15:42:43 +1200

    Build results of b90b4fd (on master)

--- lib/Dist/Zilla/Role/BundleDeps.pm
+++ lib/Dist/Zilla/Role/BundleDeps.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Role::BundleDeps;
 
-our $VERSION = '0.002002';
+our $VERSION = '0.002003';
 
 # ABSTRACT: Automatically add all plugins in a bundle as dependencies
 
@@ -112,7 +112,7 @@ Dist::Zilla::Role::BundleDeps - Automatically add all plugins in a bundle as dep
 
 =head1 VERSION
 
-version 0.002002
+version 0.002003
 
 =head1 SYNOPSIS
 
@@ -187,7 +187,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2016 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
